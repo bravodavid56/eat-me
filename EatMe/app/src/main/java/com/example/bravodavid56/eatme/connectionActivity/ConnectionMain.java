2d0416c12ced80511d5742317937c7b285970624
bravodavid56 <bravodavid56@gmail.com>
2017-08-05 23:50:46 -0700

    Added loader, but not implemented yet for data transmission.

--- EatMe/app/src/main/java/com/example/bravodavid56/eatme/connectionActivity/ConnectionMain.java
+++ EatMe/app/src/main/java/com/example/bravodavid56/eatme/connectionActivity/ConnectionMain.java
@@ -10,21 +10,28 @@ import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bravodavid56.eatme.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by bravodavid56 on 8/4/2017.
  */
 
-public class ConnectionMain extends AppCompatActivity implements  WifiP2pManager.PeerListListener , WifiP2pManager.ConnectionInfoListener{
+public class ConnectionMain extends AppCompatActivity implements  WifiP2pManager.PeerListListener , WifiP2pManager.ConnectionInfoListener,
+        LoaderManager.LoaderCallbacks {
 
     private WifiP2pManager mManager;
     private WifiP2pManager.Channel mChannel;
@@ -32,6 +39,8 @@ public class ConnectionMain extends AppCompatActivity implements  WifiP2pManager
     private final IntentFilter mIntentFilter = new IntentFilter();
     private WifiP2pDevice device = new WifiP2pDevice();
     private WifiP2pConfig config = new WifiP2pConfig();
+    private TextView tv;
+
 
     private final String TAG = "HEY YOU MADE IT ";
 
@@ -39,6 +48,8 @@ public class ConnectionMain extends AppCompatActivity implements  WifiP2pManager
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+
+        tv = (TextView) findViewById(R.id.textView);
 
         //Create the necessary manager and Channel
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
@@ -59,44 +70,47 @@ public class ConnectionMain extends AppCompatActivity implements  WifiP2pManager
 
             @Override
             public void onFailure(int reason) {
+                Toast.makeText(ConnectionMain.this, "Discovery failed. Try again.", Toast.LENGTH_SHORT).show();
 
             }
         });
 
     }
 
-
     @Override
     public void onPeersAvailable(WifiP2pDeviceList peers) {
         List<WifiP2pDevice> peerList = new ArrayList<>();
-
         peerList.addAll(peers.getDeviceList());
 
+
         if (peerList.size() > 0) {
-            WifiP2pDevice device = peerList.get(0);
-            WifiP2pConfig config = new WifiP2pConfig();
-            config.wps.setup = WpsInfo.PBC;
-            config.deviceAddress = device.deviceAddress;
-            mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+            Log.e(TAG, "onPeersAvailable: " + "      " + peerList.size() + peerList.get(0).deviceName );
+            if (peerList.get(0).primaryDeviceType.matches("^10.*")) {
+                Log.e(TAG, "onPeersAvailable: " + "TRUE AF" );
+                WifiP2pDevice device = peerList.get(0);
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.wps.setup = WpsInfo.PBC;
+                config.deviceAddress = device.deviceAddress;
+                mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        //Toast.makeText(ConnectionMain.this, "YOU ARE CONNECTED", Toast.LENGTH_LONG).show();
+                        //success logic
 
-                @Override
-                public void onSuccess() {
-                     //Toast.makeText(ConnectionMain.this, "YOU ARE CONNECTED", Toast.LENGTH_LONG).show();
-                    //success logic
-                }
+                    }
+                    @Override
+                    public void onFailure(int reason) {
+                        //failure logic
+                        Toast.makeText(ConnectionMain.this, "Connect Failed. Wait for device to accept or " +
+                                "try again. ", Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, "onFailure: "+reason );
 
-                @Override
-                public void onFailure(int reason) {
-                    //failure logic
-                }
-            });
-
-
+                    }
+                });
+            }
         }
 
-
     }
-
 
     @Override
     protected void onResume() {
@@ -111,14 +125,36 @@ public class ConnectionMain extends AppCompatActivity implements  WifiP2pManager
         unregisterReceiver(mReceiver);
     }
 
-
     @Override
     public void onConnectionInfoAvailable(WifiP2pInfo info) {
+        String loser = "Chicken Dinner.";
         if (info.isGroupOwner) {
             Toast.makeText(this, "YOU ARE THE OWNER", Toast.LENGTH_SHORT).show();
+            //tv.setText("CHICKEN DINNER");
+
         } else {
             Toast.makeText(this, "YOU ARE NOT THE OWNER", Toast.LENGTH_SHORT).show();
+            //tv.setText("CHICKEN LOSER");
         }
-        Log.e(TAG, "onConnectionInfoAvailable: "+info.toString());
+
+        Log.e(TAG, "onConnectionInfoAvailable: "+info.toString() );
+    }
+
+
+    // ========= Loader Code for Creating Socket for information transfer =============
+
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader loader, Object data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
     }
 }
