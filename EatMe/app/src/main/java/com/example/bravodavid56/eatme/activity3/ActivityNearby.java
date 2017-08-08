7a78705630f3c511040ef94a4b2c8676a76988fb
bengalletta <bgalletta81@gmail.com>
2017-08-07 17:53:08 -0700

    Updated API key to display Google Maps

--- EatMe/app/src/main/java/com/example/bravodavid56/eatme/activity3/ActivityNearby.java
+++ EatMe/app/src/main/java/com/example/bravodavid56/eatme/activity3/ActivityNearby.java
@@ -39,8 +39,8 @@ public class ActivityNearby extends FragmentActivity implements OnMapReadyCallba
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng disney = new LatLng(33.811152, -117.921971);
+        mMap.addMarker(new MarkerOptions().position(disney).title("Disneyland"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(disney));
     }
 }
