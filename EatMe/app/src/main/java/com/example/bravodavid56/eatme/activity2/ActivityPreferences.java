3d801e06c7444a4a7b7f1ea4e3b7c862cc13acef
bengalletta <bgalletta81@gmail.com>
2017-08-08 21:45:37 -0700

    Updated location marker. Updated Main Activity styling

--- EatMe/app/src/main/java/com/example/bravodavid56/eatme/activity2/ActivityPreferences.java
+++ EatMe/app/src/main/java/com/example/bravodavid56/eatme/activity2/ActivityPreferences.java
@@ -1,6 +1,8 @@
 package com.example.bravodavid56.eatme.activity2;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.location.Address;
 import android.location.Location;
 import android.net.Uri;
@@ -9,22 +11,31 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.bravodavid56.eatme.BusinessItemAdapter;
 import com.example.bravodavid56.eatme.R;
+import com.example.bravodavid56.eatme.activityRandom.ActivityRandom;
+import com.example.bravodavid56.eatme.data.Contract;
+import com.example.bravodavid56.eatme.data.DBHelper;
+import com.example.bravodavid56.eatme.data.DatabaseUtils;
 import com.example.bravodavid56.eatme.data.LocationHelper;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 /**
  * Created by bravodavid56 on 7/29/2017.
@@ -32,45 +43,36 @@ import com.google.android.gms.common.api.GoogleApiClient;
  */
 
 
-public class ActivityPreferences extends AppCompatActivity
-        implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,ActivityCompat.OnRequestPermissionsResultCallback{
+public class ActivityPreferences extends AppCompatActivity {
 
     private String TAG = "preferencesTag";
-    private Location mLastLocation;
-    private Button button;
+
     private Button startButton;
-    private TextView addressText;
     private EditText edit;
-    double latitude;
-    double longitude;
-    LocationHelper locationHelper;
+
     private Spinner spinnerPrice;
     private Spinner spinnerRating;
-    int ratings[] = {R.mipmap.stars};
-    String[] names = {};
 
+
+    private BusinessItemAdapter adapter;
+    private Cursor cursor;
+    private SQLiteDatabase db;
+    private RecyclerView recycle;
+
+    private String placeEdit;
+    private String price;
+    private String rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
-        locationHelper = new LocationHelper(this);
-        locationHelper.checkpermission();
 
-//        Spinner spin = (Spinner) findViewById(R.id.spinnerRating);
-//        spin.setOnItemSelectedListener(this);
-//
-//        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), ratings);
-//        spin.setAdapter(customAdapter);
+        recycle = (RecyclerView) findViewById(R.id.recyclerViewPreferences);
+        recycle.setLayoutManager(new LinearLayoutManager(this));
 
-        button = (Button) findViewById(R.id.locateButton);
         startButton = (Button) findViewById(R.id.enterButton);
 
-        // the layout for this activity is under res->layout->activity_preferences.xml
-        TextView logoText = (TextView) findViewById(R.id.textViewLabel);
-        logoText.setText("You are located at: ");
-        addressText = (TextView) findViewById(R.id.textView2);
 
         spinnerPrice = (Spinner) findViewById(R.id.spinnerPrice);
         spinnerRating = (Spinner) findViewById(R.id.spinnerRating);
@@ -88,47 +90,89 @@ public class ActivityPreferences extends AppCompatActivity
         spinnerRating.setAdapter(spinnerRatingAdapter);
 
 
+//        while (cursor.getCount() == 0) {
+//            cursor.close();
+//            cursor = DatabaseUtils.getAllOrderBySelection(db, placeEdit, price, rating);
+//
+//        }
 
-
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                runAddress();
-            }
-        });
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String price = spinnerPrice.getSelectedItem().toString();
-
-                String rating = spinnerRating.getSelectedItem().toString();
-
-                String placeEdit = edit.getText().toString();
-
-                Intent sendDataToFragment = new Intent(ActivityPreferences.this, FragmentPreferences.class);
+                placeEdit = edit.getText().toString();
 
 
-                Bundle args = new Bundle();
-                args.putString("Place", placeEdit);
-                args.putString("Price", price);
-                args.putString("Rating", rating);
+                price = spinnerPrice.getSelectedItem().toString();
+
+                rating = spinnerRating.getSelectedItem().toString();
 
 
-//                sendDataToFragment.putExtra("Place", placeEdit);
-//                sendDataToFragment.putExtra("Price", price);
-//                sendDataToFragment.putExtra("Rating", rating);
-//                startActivity(sendDataToFragment);
+                db = new DBHelper(ActivityPreferences.this).getReadableDatabase();
+                cursor = DatabaseUtils.getAllOrderBySelection(db, placeEdit, price, rating);
 
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentPreferences frag = new FragmentPreferences();
-                frag.setArguments(args);
-                frag.show(fm, "FragmentPreferences");
+
+
+                LinearLayout preferencesPopup = (LinearLayout) findViewById(R.id.popupPreferences);
+                preferencesPopup.setVisibility(View.INVISIBLE);
+
+                Log.i("Place", placeEdit);
+                Log.i("Price", price);
+                Log.i("Rating", rating);
+
+                adapter = new BusinessItemAdapter(cursor);
+                recycle.setAdapter(adapter);
             }
         });
+    }
+
+
+    //Intent sendDataToFragment = new Intent(ActivityPreferences.this, FragmentPreferences.class);
+
+
+//                db = new DBHelper(ActivityPreferences.this).getReadableDatabase();
+//                cursor = DatabaseUtils.getAllByPrice(db,price);
+//                adapter = new BusinessItemAdapter(cursor);
+
+
+//                Bundle args = new Bundle();
+//                args.putString("Place", placeEdit);
+//                args.putString("Price", price);
+//                args.putString("Rating", rating);
+//
+//
+//                FragmentManager fm = getSupportFragmentManager();
+//                FragmentPreferences frag = new FragmentPreferences();
+//                frag.setArguments(args);
+//                frag.show(fm, "FragmentPreferences");
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        db = new DBHelper(ActivityPreferences.this).getReadableDatabase();
+        cursor = DatabaseUtils.getAll(db);
+        adapter = new BusinessItemAdapter(cursor);
+        recycle.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        db.close();
+        cursor.close();
+    }
+
+}
+
+
+
+
+
+
+
+
 
 //        textView.setText("You clicked Preferences");
 //        startButton.setOnClickListener(new View.OnClickListener() {
@@ -152,152 +196,21 @@ public class ActivityPreferences extends AppCompatActivity
 //            }
 //        });
 //
-//        // check availability of play services
-        if (locationHelper.checkPlayServices()) {
-            // Building the GoogleApi client
-            locationHelper.buildGoogleApiClient();
-        }
 
 
-    }
-
-//
-//    //Performing action onItemSelected and onNothing selected
-//    @Override
-//    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
-//        Toast.makeText(getApplicationContext(), names[position], Toast.LENGTH_LONG).show();
-//    }
-//
-//    @Override
-//    public void onNothingSelected(AdapterView<?> arg0) {
-//    }
-
-public void runAddress(){
-    mLastLocation = locationHelper.getLocation();
-
-    if (mLastLocation != null) {
-        latitude = mLastLocation.getLatitude();
-        longitude = mLastLocation.getLongitude();
-        getAddress();
-
-    } else {
-//            start = false;
-        if(button.isEnabled())
-            button.setEnabled(false);
-
-        showToast("Couldn't get the location. Make sure location is enabled on the device");
-    }
-
-}
 
 
-    public void getAddress() {
-        Address locationAddress;
-
-        locationAddress = locationHelper.getAddress(latitude, longitude);
-
-        if (locationAddress != null) {
-
-            String address = locationAddress.getAddressLine(0);
-            String address1 = locationAddress.getAddressLine(1);
-            String city = locationAddress.getLocality();
-            String state = locationAddress.getAdminArea();
-            String country = locationAddress.getCountryName();
-            String postalCode = locationAddress.getPostalCode();
 
 
-            String currentLocation;
-            String addr;
-
-            if (!TextUtils.isEmpty(address)) {
-                currentLocation = address;
-                addr = address;
-
-//                if (!TextUtils.isEmpty(address1))
-//                    currentLocation += "\n" + address1;
-
-                if (!TextUtils.isEmpty(city)) {
-                    currentLocation += "\n" + city;
-                    addr += " " + city;
-
-                    if (!TextUtils.isEmpty(postalCode))
-                        currentLocation += " " + postalCode;
-                        addr += " " + postalCode;
-                } else {
-                    if (!TextUtils.isEmpty(postalCode))
-                        currentLocation += "\n" + postalCode;
-                        addr += " " + postalCode;
-                }
-
-                if (!TextUtils.isEmpty(state))
-                    currentLocation += "\n" + state;
-                    addr += " " + state;
-
-                if (!TextUtils.isEmpty(country))
-                    currentLocation += "\n" + country;
-                    addr += " " + country;
-                Log.d(addr,"check");
-                addressText.setText(currentLocation);
-                addressText.setVisibility(View.VISIBLE);
-                Log.d(TAG, "Address");
-                Log.d(currentLocation, "here");
-
-                if (!button.isEnabled()) {
-                    button.setEnabled(true);
-                }
-
-            } else
-                showToast("Something went wrong");
-        }
-    }
-
-    public void showToast(String message)
-    {
-        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        locationHelper.onActivityResult(requestCode,resultCode,data);
-    }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        locationHelper.checkPlayServices();
-    }
-
-    /**
-     * Google api callback methods
-     */
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        Log.i("Connection failed:", " ConnectionResult.getErrorCode() = "
-                + result.getErrorCode());
-    }
-
-    @Override
-    public void onConnected(Bundle arg0) {
-
-        // Once connected with google api, get the location
-
-        mLastLocation=locationHelper.getLocation();
-    }
-
-    @Override
-    public void onConnectionSuspended(int arg0) {
-        locationHelper.connectApiClient();
-    }
-
-    // Permission check functions
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        // redirects to utils
-        locationHelper.onRequestPermissionsResult(requestCode,permissions,grantResults);
-
-    }
 
 
-}
+
+
+
+
+
+
+
+
